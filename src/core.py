37a253a2e2c9e0f8cd7c05343eefd31c378eaed2
Jean-Baptiste Musso <jbmusso@gmail.com>
2022-07-11 17:55:23 +0200

    Implement simple Persons DSL

--- src/core.py
+++ src/core.py
@@ -1,6 +1,39 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Generic, Type, TypeVar
+from pyspark.sql.dataframe import DataFrame
+
+
 def say_hello(content):
     return f"Hello World! {content}"
 
 
 def count_lines(df):
     return df.count()
+
+
+@dataclass
+class Person:
+    name: str
+    gender: str
+
+
+TPersonsDSL = TypeVar("TPersonsDSL", bound="PersonsDSL")
+
+
+class PersonsDSL:
+    def __init__(self, df: DataFrame[Person]):
+        self.df = df
+
+    def females(self: TPersonsDSL) -> TPersonsDSL:
+        return PersonsDSL(self.df.filter(self.df.gender == "female"))
+
+    def names(self: TPersonsDSL) -> TPersonsDSL:
+        return PersonsDSL(self.df.select("name"))
+
+    def gender(self: TPersonsDSL) -> TPersonsDSL:
+        return PersonsDSL(self.df.select("gender"))
+
+    def count(self: TPersonsDSL) -> int:
+        return self.df.count()
--- src/main_test.py
+++ src/main_test.py
@@ -1,5 +1,5 @@
 from pyspark import Row
-from core import count_lines, say_hello
+from core import PersonsDSL, count_lines, say_hello
 
 from datetime import datetime, date
 from pyspark.sql import SparkSession
@@ -8,6 +8,31 @@ from pyspark.sql import Row
 
 def test_hello_world():
     assert say_hello("Bob") == "Hello World! Bob"
+
+
+def test_person_domain_specific_language():
+    spark = SparkSession.builder.getOrCreate()
+
+    df = spark.createDataFrame(
+        [
+            Row(
+                name="Alice",
+                gender="female",
+            ),
+            Row(
+                name="Bob",
+                gender="male",
+            ),
+            Row(
+                name="Carol",
+                gender="female",
+            ),
+        ]
+    )
+
+    persons = PersonsDSL(df).females().gender()
+
+    assert persons.count() == 2
 
 
 def test_count_lines():
