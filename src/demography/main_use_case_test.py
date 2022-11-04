68f6dc42a9cb27be9bd11e9d1bae7f88845f6dbd
Jean-Baptiste Musso <jbmusso@gmail.com>
2022-11-04 13:18:10 +0100

    Remove sample.py file and related tests which were simply used to bootstrap the git repository

--- src/demography/main_use_case_test.py
+++ src/demography/main_use_case_test.py
@@ -1,17 +1,9 @@
-from datetime import datetime, date
-
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 
-from demography.core.sample import count_lines, say_hello
-from demography.core.use_cases.average_age import AverageAgeUseCase
 from demography.secondary_adapters.repositories.person.in_memory_person_repository import (
     InMemoryPersonRepository,
 )
-
-
-def test_hello_world():
-    assert say_hello("Bob") == "Hello World! Bob"
 
 
 def test_count_female_persons(spark_session: SparkSession):
@@ -28,33 +20,3 @@ def test_find_first_people_named_alice(spark_session: SparkSession):
     alices = repository.find_all_persons().named("Alice").gender().df.collect()[0]
 
     assert alices == Row(gender="female")
-
-
-def test_count_lines(spark_session: SparkSession):
-    df = spark_session.createDataFrame(
-        [
-            Row(
-                a=1,
-                b=2.0,
-                c="string1",
-                d=date(2000, 1, 1),
-                e=datetime(2000, 1, 1, 12, 0),
-            ),
-            Row(
-                a=2,
-                b=3.0,
-                c="string2",
-                d=date(2000, 2, 1),
-                e=datetime(2000, 1, 2, 12, 0),
-            ),
-            Row(
-                a=4,
-                b=5.0,
-                c="string3",
-                d=date(2000, 3, 1),
-                e=datetime(2000, 1, 3, 12, 0),
-            ),
-        ]
-    )
-
-    assert count_lines(df) == 3
