1da622d461a1a126e5a320456fed8c1182aa2d4d
Jean-Baptiste Musso <jbmusso@gmail.com>
2022-11-06 09:33:57 +0100

    Add test reading from gitignored kaggle_movies_dataset

--- src/cinema/primary_adapters/tests/most_expensive_movies_use_case_test.py
+++ src/cinema/primary_adapters/tests/most_expensive_movies_use_case_test.py
@@ -1,13 +1,15 @@
-from pyspark.sql import SparkSession
 from cinema.core.domain.movie_type import Movie
 from cinema.core.ports.primary.most_expensive_movie_command import (
     MostExpensiveMoviesCommand,
 )
-
 from cinema.core.use_cases.most_expensive_movies import MostExpensiveMoviesUseCase
+from cinema.secondary_adapters.repositories.movie.file_system_movie_repository import (
+    FileSystemMovieRepository,
+)
 from cinema.secondary_adapters.repositories.movie.in_memory_movie_repository import (
     InMemoryMovieRepository,
 )
+from pyspark.sql import SparkSession
 
 
 class TestMostExpensiveMoviesUseCase:
@@ -25,3 +27,14 @@ class TestMostExpensiveMoviesUseCase:
                 budget=205_000_000,
             )
         ]
+
+    def test_most_expensive_movies_kaggle_dataset(self, spark_session: SparkSession):
+        movie_repository = FileSystemMovieRepository(spark_session)
+        most_expensive_movies_use_case = MostExpensiveMoviesUseCase(movie_repository)
+
+        most_expensive_movie_ever_command = MostExpensiveMoviesCommand(count=1)
+
+        [movie] = most_expensive_movies_use_case.run(most_expensive_movie_ever_command)
+
+        assert movie.get("name") == "Pirates of the Caribbean: On Stranger Tides"
+        assert movie.get("budget") == 380_000_000
